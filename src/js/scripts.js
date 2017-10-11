693d3461cd84dcf9830f092203add6b3b6b249c8
hifilorau <tomrau@gmail.com>
2017-10-11 15:53:16 -0400

    checkin

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,41 +1,25 @@
 app.fadeIn = function () {
 
-	$(".p-2").hide();
-	$('.flash').hide();
-	$('.flash-3').hide();
-	$('.robot-fobot').hide();
+	var heroTextShow = function() {
+	  var elements = $('.hero-item'),
+	      total    = elements.length,
+	      index    = 0;
+	      time = 5000;
 
-	setTimeout(function() {
-		  $('.flash-3').hide();
-	}, 4650);
+	  (function next() {
+	    elements.eq(index)
+	            .fadeIn(500)
+	            .delay(time)
+	            .fadeOut(500, function() {
+	                index++;
+	                time = 2000;
+	                if (index >= total) { index = 0; time=5000; }
+	                next();
+	            });
+	  })();
+	};
 
-	setTimeout(function() {
- $( ".p-1" ).toggle( "pulsate" );
-		setTimeout(function(){
-		  $('.flash-3').toggle('pulsate', {times: 5}, 100);
-		}, 200);
-		 setTimeout(function() {
-				$('.flash-3').hide()
-				$('.flash-2').hide()
-				$('.flash').show();
-				$(".p-1").hide();
-			}, 400);
-	}, 2700);
-
-//	         $('.robot-fobot').css('opacity', 0);
-		setTimeout(function() {
-
-			setTimeout(function() {
-//				$( ".hero-content" ).toggle( "pulsate", {times: 10}, 300);
-				$( ".p-2" ).fadeIn(5000);
-		    $('.robot-fobot').hide();
-			  $('.flash').fadeOut(2500);
-				$('.flash-3').hide();
-//		    $('.p-2').show();
-				$('.p-1').hide();
-			}, 300);
-
-		}, 2900 );
+	heroTextShow();
 //	$(".p-1").hide();
 
 
